6f51607912f114abe03c64c17a4d474709d4f492
ParangBird <seekerman@naver.com>
2022-08-31 22:40:18 +0900

    Initial commit

---
+++ src/main/java/sotree/dia/DiaApplication.java
@@ -0,0 +1,13 @@
+package sotree.dia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sotree/dia/DiaApplicationTests.java
@@ -0,0 +1,13 @@
+package sotree.dia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
