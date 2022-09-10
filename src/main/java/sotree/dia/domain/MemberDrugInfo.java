f6e60b7ad7f14a20a7abb2ca6b14557531a58e43
ParangBird <seekerman@naver.com>
2022-09-10 21:26:21 +0900

    entity 매핑 수정 - Drug : Member -> Drug : MemberDrugInfo : Member

--- src/main/java/sotree/dia/domain/MemberDrugInfo.java
+++ src/main/java/sotree/dia/domain/MemberDrugInfo.java
@@ -2,10 +2,12 @@ package sotree.dia.domain;
 
 import javax.persistence.*;
 
-@Table(name = "drug")
+@Table(name = "memberDrugInfo")
 @Entity
-public class Drug {
+public class MemberDrugInfo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private Long memberId;
+    private Long drugId;
 }
