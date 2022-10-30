c33265930bfeec9770b277c6b4bef5807f2bfe4e
ParangBird <seekerman@naver.com>
2022-10-30 15:59:03 +0900

    feat: /api/image/{drugId} 로 이미지 쏴주기(예외처리 해야함)

--- src/main/java/sotree/dia/controller/DrugInfoController.java
+++ src/main/java/sotree/dia/controller/DrugInfoController.java
@@ -1,35 +1,51 @@
 package sotree.dia.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import sotree.dia.domain.entity.DrugInfo;
 import sotree.dia.service.DrugInfoService;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class DrugInfoController {
     private final DrugInfoService drugInfoService;
+
     @GetMapping("/api/drug/test")
-    public DrugInfo helloDrug(){
+    public DrugInfo helloDrug() {
         DrugInfo drugInfo = drugInfoService.findByDrugId(200300406L);
         return drugInfo;
     }
+
     @GetMapping("/api/drug/nameSearch")
-    public List<DrugInfo> nameSearch(@RequestParam String query){
+    public List<DrugInfo> nameSearch(@RequestParam String query) {
         List<DrugInfo> drugInfos = drugInfoService.findByDrugNameContains(query);
         return drugInfos;
     }
+
     @GetMapping("/api/drug/ingredientSearch")
-    public List<DrugInfo> ingredientSearch(@RequestParam String query){
+    public List<DrugInfo> ingredientSearch(@RequestParam String query) {
         List<DrugInfo> drugInfos = drugInfoService.findByDrugIngredientContains(query);
         return drugInfos;
     }
+
     @GetMapping("/api/drug/textSearch")
-    public List<DrugInfo> textSearch(@RequestParam String query){
+    public List<DrugInfo> textSearch(@RequestParam String query) {
         List<DrugInfo> drugInfos = drugInfoService.findByDrugNameAndDrugIngredientContains(query);
         return drugInfos;
+}
+
+    @GetMapping(value = "/api/image/{drugId}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public byte[] drugImage(@PathVariable("drugId") String drugId) throws IOException {
+        InputStream in = getClass().getResourceAsStream("/images/" + drugId + ".jpg");
+        return IOUtils.toByteArray(in);
     }
 }
--- src/main/java/sotree/dia/controller/HelloController.java
+++ src/main/java/sotree/dia/controller/HelloController.java
@@ -1,11 +1,14 @@
 package sotree.dia.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Slf4j
 @RestController
@@ -14,5 +17,10 @@ public class HelloController {
     public String hello(HttpServletRequest request){
         log.info("hello {}", request.getRemoteHost());
         return "hello";
+    }
+    @GetMapping(value = "/image-test", produces = MediaType.IMAGE_PNG_VALUE)
+    public byte[] hello() throws IOException {
+        InputStream in = getClass().getResourceAsStream("/images/logo.png");
+        return IOUtils.toByteArray(in);
     }
 }
