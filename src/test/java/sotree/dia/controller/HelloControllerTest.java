82314a960cb6a7709b60edbc44d50bdf6f0883c2
ParangBird <seekerman@naver.com>
2023-02-16 02:44:59 +0900

    test: HelloController test

---
+++ src/test/java/sotree/dia/controller/HelloControllerTest.java
@@ -0,0 +1,23 @@
+package sotree.dia.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(HelloController.class)
+@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+class HelloControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void mockMvcTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+    }
+}
